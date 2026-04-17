db96404ce1a7128b8f9077c1d700fdc1a8c1ee26
Tonmoy Pul <tonmoy7563@gmail.com>
2026-04-17 10:16:11 +0600

    first commit

---
+++ PythonJourney/PythonFromFreeCodeCamp/PythonBasics/IntroductionToStrings/stringImmutability.py
@@ -0,0 +1,43 @@
+myStr1 = 'Hello'
+myStr2 = "World!"
+
+#MultiLine String
+myStr3 = '''This is a multiline
+string 
+with triple 
+single 
+quote sign'''
+
+myStr4 = """This is also 
+a multiline string 
+with three 
+double 
+quote sign"""
+
+print(myStr1)
+print(myStr2)
+print(myStr3)
+print(myStr4)
+
+#When a string contains single or double quote sign
+mtStr5 = "It's a single line string"
+myStr6 =  'She said, "Hello World!"'
+
+#when you want to escape the quotation use \ ( backslash)
+msg = 'It\'s a sunny day'
+quote = "She said, \"Hello World!\""
+
+uniStr = 'Hello World!'
+print('gf' in uniStr)
+print('Hello' in uniStr)
+print("World" in uniStr)
+print(len(uniStr))
+
+#Selecting any word using index
+print(uniStr[-1], uniStr[-2], uniStr[-3], uniStr[-4], uniStr[-5], uniStr[-6], uniStr[-7], uniStr[-8], uniStr[-9], uniStr[-10], uniStr[-11], uniStr[-12])
+print(uniStr[0], uniStr[1], uniStr[2], uniStr[3], uniStr[4], uniStr[5], uniStr[6], uniStr[7], uniStr[8], uniStr[9], uniStr[10], uniStr[11])
+
+#Reassigning value to a variable
+greet = 'Hey'
+greet = 'Hello'
+print(greet)
