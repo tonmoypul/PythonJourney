fd410df596fb1d8bbdca6256327d7ea0bf26ee7b
Tonmoy Pul <tonmoy7563@gmail.com>
2026-04-24 14:39:27 +0600

    finished learning to work with integers and floats, except to converting them

---
+++ PythonJourney/PythonFromFreeCodeCamp/PythonBasics/NumbersAndMathematicalOperations/workingWithIntegerAndFloats.py
@@ -0,0 +1,53 @@
+##Sum, subtraction, multiplication, division, modulo, floor and exponentiation of Integers and Floats
+
+##Integers
+myInt1 = 34
+myInt2 = 7
+print(f'Integers {myInt1}, {myInt2}')
+#Sum
+sunmation = myInt1 + myInt2
+print("Sum of two integers: ", sunmation)
+#Subtraction
+difference = myInt1 - myInt2
+print("Subtraction of two integers: ", difference)
+#Multiplication
+multiplication = myInt1 * myInt2
+print("Multiplication of two integers: ", multiplication)
+#Division
+division = myInt1 / myInt2
+print("Division of two integers: ", division)
+#Modulo
+modulo = myInt1 % myInt2
+print("Modulo of two integers: ", modulo)
+#Floor
+floor = myInt1 // myInt2
+print("Floor of two integers: ", floor)
+#Exponentiation
+exp = myInt1 ** myInt2
+print("Exponentiation of two integers: ", exp)
+
+##Floats
+myFloat1 = 3.54
+myFloat2 = 3.12
+print(f'Floats {myFloat1}, {myFloat2}')
+#Sum
+floatSum = myFloat1 + myFloat2
+print(f'Sum of two floats: ', floatSum)
+#Subtraction
+floatDifference = myFloat1 - myFloat2
+print(f'Subtraction of two floats: ', floatDifference)
+#Multiplication
+floatMultiple = myFloat1 * myFloat2
+print(f'Multiplication of two floats: ', floatMultiple)
+#Division
+floatDivision = myFloat1 / myFloat2
+print(f'Division of two floats: ', floatDivision)
+#Modulo
+floatModulo = myFloat1 % myFloat2
+print(f'Modulo of two floats: ', floatModulo)
+#Floor
+floatFloor = myFloat1 // myFloat2
+print(f'Floor of two floats: ', floatFloor)
+#Exponentiation
+floatExp = myFloat1 ** myFloat2
+print(f'Exponentiation of two float: ', floatExp)
