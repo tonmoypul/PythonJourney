ea3b546937c08e90ea80ea341500339b204fc3ee
Tonmoy Pul <tonmoy7563@gmail.com>
2026-07-24 14:11:30 +0600

    I have done the 1st kec of the booleans and conditonals part of python basics from fcc

---
+++ PythonJourney/PythonFromFreeCodeCamp/PythonBasics/BooleansAndConditionals/howDoConditonalWokrk.py
@@ -0,0 +1,10 @@
+age = 67
+
+if age >= 18:
+    print("You are an Adult now!!")
+elif age >=30:
+    print("You are at your prime age now")
+elif age >= 60:
+    print("You are a senior citizen now")
+else:
+    print("You are not eligible to get a complement son")
