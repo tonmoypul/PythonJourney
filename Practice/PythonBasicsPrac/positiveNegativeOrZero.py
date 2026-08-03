0d36a0afcb37d4d68148f894220dc6003047acff
Tonmoy Pul <tonmoy7563@gmail.com>
2026-08-04 00:00:08 +0600

    completdd this one

---
+++ Practice/PythonBasicsPrac/positiveNegativeOrZero.py
@@ -0,0 +1,12 @@
+#Given a number
+#Determine whether it is positive, negative or zero
+#
+#
+
+num = 637.9
+if num == 0:
+  print("It is zero")
+elif num < 0:
+  print("It's negative")
+else:
+  print("It's positive")
